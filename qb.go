8e82b8b5f0121dee09225cec51e05bc4be183eb4
Karlo Batrla <karlo.batrla@aduro.hr>
2017-06-19 11:45:50 +0200

    Added bulk insert query builder

--- qb.go
+++ qb.go
@@ -46,7 +46,52 @@ func QueryBuilder(query string, definition []Definition) (string, []interface{})
 	return query, requestArgs
 }
 
+// BulkInsert create insert statement
+func BulkInsert(query string, rows []interface{}) (string, []interface{}, error) {
+	var err error
+
+	if len(rows) == 0 {
+		err = fmt.Errorf("No rows in request")
+		return query, nil, err
+	}
+
+	placeholder, count := createPlaceholder(rows[0])
+
+	placeholders := make([]string, len(rows))
+	args := make([]interface{}, (len(rows) * count))
+
+	var argCount int
+	for i, entry := range rows {
+		placeholders[i] = placeholder
+		v := reflect.ValueOf(entry)
+		for y := 0; y < v.NumField(); y++ {
+			args[argCount] = v.Field(y).Interface()
+			argCount++
+		}
+	}
+
+	statement := fmt.Sprintf("%s %s", query, strings.Join(placeholders, ","))
+
+	return statement, args, err
+}
+
 // isZero check if interface equals zero value of its type
 func isZero(x interface{}) bool {
 	return x == reflect.Zero(reflect.TypeOf(x)).Interface()
+}
+
+// createPlaceholder create placeholder for one insert on structure
+func createPlaceholder(a interface{}) (string, int) {
+
+	instance := reflect.TypeOf(a)
+	fCount := instance.NumField()
+
+	var ph []string
+	for i := 1; i <= fCount; i++ {
+		ph = append(ph, "?")
+	}
+
+	placeholder := fmt.Sprintf("(%s)", strings.Join(ph, ","))
+
+	return placeholder, fCount
 }
--- qb_test.go
+++ qb_test.go
@@ -22,3 +22,34 @@ func TestQueryBuilder(t *testing.T) {
 	fmt.Println(result)
 	fmt.Println(args)
 }
+
+type TestStruct struct {
+	Id    int
+	Name  string
+	Other string
+}
+
+func TestBulkInsert(t *testing.T) {
+
+	var request []interface{}
+	query := "insert into test (col1, col2, col3) values"
+
+	request = append(request,
+		TestStruct{Id: 1, Name: "a", Other: "haha"},
+		TestStruct{Other: "Hehe"},
+		TestStruct{Id: 3, Name: "c", Other: "hihi"},
+		TestStruct{Id: 4, Other: "hoho"},
+		TestStruct{Id: 5, Name: "e", Other: "huhu"},
+		TestStruct{Id: 6, Name: "f", Other: "=U="},
+	)
+
+	q, args, err := BulkInsert(query, request)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Query: ", q)
+
+	for i, a := range args {
+		fmt.Println(i+1, a)
+	}
+}
